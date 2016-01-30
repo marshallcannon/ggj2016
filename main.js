e3dd81378a440b6e70818c28dd51dc8af23c3d15
marshallcannon <marshcannon@gmail.com>
2016-01-29 23:04:59 -0700

    Setup node modules

---
+++ main.js
@@ -0,0 +1,17 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+
+  game.load.image('player', 'assets/player.png');
+
+}
+
+var player;
+
+function create() {
+  
+}
+
+function update() {
+
+}
